ab4f93a528f00076760cd2a01ea25e850f8c3902
Konstantin Geldard <konstantin.geldard@outlook.com>
2019-06-21 14:17:20 +1000

    Update DNStoIP.ps1

--- DNStoIP.ps1
+++ DNStoIP.ps1
@@ -1,4 +1,4 @@
-﻿$filename = .\DNS.txt # File in current working directory.
+$filename = .\DNS.txt # File in current working directory.
 $csv_file = .\DNS_TO_IP.csv # File in current working directory
 
 # Doing cleanup. We want to append to a fresh CSV.
@@ -6,7 +6,7 @@ Remove-Item –path $csv_file
 
 # read and iterate over each line in $filename
 foreach($line in Get-Content $filename) {
-    try{
+    try {
         $resolution = Resolve-DnsName -Name $line -ErrorAction Stop 
     } catch {
         # We set manual parameters for columns due to error resolving the name.
